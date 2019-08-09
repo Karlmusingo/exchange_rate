c32fd4cd1aa481c03423da289fdda02dd1f02fd1
karlmusingo <karlemusingo@gmail.com>
2019-08-09 11:36:22 +0200

    Fetching exchange rate from a sandbox GraphQL API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Exchange rate using GraphQL and Apollo Client</p>
       </header>
+      <Query
+        query={gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+              name
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <p> Loading...</p>;
+          if (error) return <p> Error :(</p>;
+
+          return data.rates.map(({ currency, rate, name }) => {
+            return (
+              <div key={currency}>
+                <p>
+                  {name} => {currency} : {rate}
+                </p>
+              </div>
+            );
+          });
+        }}
+      </Query>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { gql } from "apollo-boost";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
